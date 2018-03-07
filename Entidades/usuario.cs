ce31364c64dba9e8e7084c452a2a6115067e6bc6
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2018-03-06 20:52:15 -0600

    chaanges made to presentation layer

---
+++ Entidades/usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class usuario
+    {
+        public string usuario_id { set; get; }
+        public string nombreUsuario { set; get;}
+
+        public string nombreCompleto { set; get; }
+        public rol rolUsuario { set; get; }
+        public string clave { set; get; }
+        public bool activo { set; get; }
+
+
+        public usuario()
+        {
+
+        }
+
+        public usuario(string usuario_idp, string nombre, string nombreCompletop, rol rolp, string clavep, bool activop)
+        {
+            this.usuario_id = usuario_idp;
+            this.nombreUsuario = nombre;
+            this.nombreCompleto = nombreCompletop;
+            this.rolUsuario = rolp;
+            this.clave = clavep;
+            this.activo = activop;
+        }
+    }
+}
