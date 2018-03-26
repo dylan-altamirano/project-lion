fb9ec09faa4bc19acc3d7a0eea30cadfbb6aeb40
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2018-03-25 18:50:23 -0600

    changes made to Usuario in all layers

--- Datos/UsuarioDato.cs
+++ Datos/UsuarioDato.cs
@@ -5,16 +5,20 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
 
 namespace Datos
 {
     public class UsuarioDato
     {
-        public static SqlDataReader SeleccionarUsuario()
+        public static SqlDataReader SeleccionarUsuario(string nombre)
         {
             Database db = DatabaseFactory.CreateDatabase("Default");
             SqlCommand comando = new SqlCommand("sp_obtener_usuario");
             comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@nombreUsuario", nombre);
+
             SqlDataReader reader = db.ExecuteReader(comando);
             return reader;
         }
@@ -24,16 +28,50 @@ namespace Datos
         /// los datos guardados en la base de datos.
         /// </summary>
         /// <returns></returns>
-        public static SqlDataReader AutenticarUsuario()
+        public static SqlDataReader AutenticarUsuario(usuario Usuario)
         {
             Database db = DatabaseFactory.CreateDatabase("Default");
             SqlCommand comando = new SqlCommand("sp_autenticar_usuario");
             comando.CommandType = CommandType.StoredProcedure;
 
-           
+            comando.Parameters.AddWithValue("@Usuario", Usuario.nombreUsuario);
+            comando.Parameters.AddWithValue("@passwordp", Usuario.clave);
 
             SqlDataReader reader = db.ExecuteReader(comando);
             return reader;
+        }
+
+        //Inserta un nuevo alquiler
+        public static void Insertar(usuario Usuario)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+            SqlCommand comando = new SqlCommand("sp_crear_usuario");
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@usuario_id", Usuario.usuario_id);
+            comando.Parameters.AddWithValue("@nombre", Usuario.nombreUsuario);
+            comando.Parameters.AddWithValue("@nombreCompleto", Usuario.nombreCompleto);
+            comando.Parameters.AddWithValue("@rol", Usuario.rolUsuario);
+            comando.Parameters.AddWithValue("@clave", Usuario.clave);
+            comando.Parameters.AddWithValue("@activo", Usuario.activo);
+           
+
+            db.ExecuteNonQuery(comando);
+        }
+        /*Sentencia del procedimiento almacenado
+         */
+        public static void Modificar(usuario Usuario)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+            SqlCommand comando = new SqlCommand("sp_modificar_usuario");
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@usuario_id", Usuario.usuario_id);
+            comando.Parameters.AddWithValue("@nombre", Usuario.nombreUsuario);
+            comando.Parameters.AddWithValue("@nombreCompleto", Usuario.nombreCompleto);
+            comando.Parameters.AddWithValue("@rol", Usuario.rolUsuario);
+            comando.Parameters.AddWithValue("@clave", Usuario.clave);
+            comando.Parameters.AddWithValue("@activo", Usuario.activo);
+
+            db.ExecuteNonQuery(comando);
         }
     }
 }
---
+++ LoginaNegocio/UsuarioLN.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Datos;
+using System.Data.SqlClient;
+
+namespace LoginaNegocio
+{
+    public class UsuarioLN
+    {
+        public static usuario SeleccionarUsuario(string nombre)
+        {
+            usuario Usuario = null;
+
+            SqlDataReader data = UsuarioDato.SeleccionarUsuario(nombre);
+
+            while (data.Read())
+            {
+                Usuario = new usuario();
+
+                Usuario.usuario_id = data["usuario_id"].ToString();
+                Usuario.nombreUsuario = data["nombre"].ToString();
+                Usuario.nombreCompleto = data["nombreCompleto"].ToString();
+                Usuario.rolUsuario = (rol)Enum.Parse(typeof(rol), data["rol"].ToString());
+                Usuario.activo = Convert.ToBoolean(data["activo"]);
+            }
+
+            return Usuario;
+        }
+
+        public static usuario AutenticarUsuario(usuario user)
+        {
+            usuario Usuario = null;
+
+            SqlDataReader data = UsuarioDato.AutenticarUsuario(user);
+
+            while (data.Read())
+            {
+                Usuario = new usuario();
+
+                Usuario.usuario_id = data["usuario_id"].ToString();
+                Usuario.nombreUsuario = data["nombre"].ToString();
+                Usuario.nombreCompleto = data["nombreCompleto"].ToString();
+                Usuario.rolUsuario = (rol)Enum.Parse(typeof(rol), data["rol"].ToString());
+                Usuario.activo = Convert.ToBoolean(data["activo"]);
+            }
+
+            return Usuario;
+        }
+
+        public static void Nuevo(usuario Usuario)
+        {
+            if (UsuarioDato.SeleccionarUsuario(Usuario.nombreUsuario) != null)
+            {
+                UsuarioDato.Insertar(Usuario);
+            }
+        }
+
+        public static void Modificar(usuario Usuario)
+        {
+            UsuarioDato.Modificar(Usuario);
+        }
+    }
+}
